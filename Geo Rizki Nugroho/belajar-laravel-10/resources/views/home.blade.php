20dd547bcd2ab5709d7e7f8a67f211d2574ff0cc
GeoRizkiNugroho <geosuryani@gmail.com>
2024-09-18 15:30:39 +0700

    Tugas  18-09-2024

---
+++ Geo Rizki Nugroho/belajar-laravel-10/resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+@section('content')
+
+
+<!-- Content Section -->
+
+
+<!-- Featured Manhwa Section -->
+<section class="featured">
+    <h2>Featured Manhwa</h2>
+    <div class="manhwa-card">
+        <img src="assets/img/manhwa1.png" alt="Manhwa 1">
+        <h3>Magic Emperor</h3>
+        <p>“Zhuo Yifan adalah seorang kaisar sihir atau bisa di panggil kaisar iblis, karena dia mempunyai buku kaisar kuno yang di sebut buku sembilan rahasia dia menjadi sasaran semua ahli beradiri bahkan dia di khianati dan di bunuh oleh muridnya. Kemudian jiwanya masuk dan hidup kembali dalam seorang anak pelayan keluarga bernama Zhuo Fan.Karena suatu sihir iblis mengekangnya, dia harus menyatukan ingatan anak itu dan tidak bisa mengabaikan keluarga dan nona yang dia layaninya. Bagaimana kehidupan nya membangun kembali keluarganya…”</p>
+    </div>
+    <div class="manhwa-card">
+        <img src="assets/img/manhwa2.png" alt="Manhwa 2">
+        <h3>Solo Leveling</h3>
+        <p>E-class hunter Jinwoo Sung is the weakest of them all. Looked down on by everyone, he has no money, no abilities to speak of, and no other job prospects. So when his party finds a hidden dungeon, he’s determined to use this chance to change his life for the better…but the opportunity he finds is a bit different from what he had in mind…Original Webtoon:KakaoPage, DaumOfficial English Translation:Yen Press (Volumes), Tappytoon, Pocket Comics, Tapas, Webnovel,Official Translations:Japanese, Thai, Indonesian, S.Chinese, T.Chinese, German.</p>
+    </div>
+    <div class="manhwa-card">
+        <img src="assets/img/manhwa3.png" alt="Manhwa 3">
+        <h3>Mercenary Enrollment</h3>
+        <p>Yu Ijin adalah satu-satunya yang selamat dari kecelakaan pesawat saat dia masih kecil. Setelah menjadi tentara bayaran untuk bertahan hidup selama 10 tahun, ia kembali ke keluarganya di kampung halamannya. Kemudian dia bertemu adiknya dan besekolah bersama adik perempuan dan dia akan menghadapi berbagai masalah dari masa lalu maupun masa depan dengan pengalamannya sebagai tentara bayarannya, nantikan apa Yu Ijin akan bertahan dan terus maju atau kah dia akan terbunuh ditengah tengah perjuangannya</p>
+    </div>
+</section>
+@endsection
